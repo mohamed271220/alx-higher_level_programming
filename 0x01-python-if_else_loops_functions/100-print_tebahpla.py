3defa3de68f73d3f685cd5d7d25df9a4c77e5b40
mohamed <mohamedaprahym@gmail.com>
2024-03-12 14:54:32 +0200

    feat

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,2 +1,3 @@
 #!/usr/bin/python3
-print("".join([chr(i - 32) if i % 2 == 1 else chr(i) for i in range(122, 96, -1)]), end="")
+for i in range(122, 96, -1):
+    print("{:c}".format(i - 32 if i % 2 else i), end="")
