116c5d7eafb26199a5078f1858cc43f2dcfe1b33
mohamed <mohamedaprahym@gmail.com>
2024-03-15 17:40:56 +0200

    d

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,7 +7,7 @@ def main():
     if num_args == 0:
         print("0 arguments.")
     elif num_args == 1:
-        print("1 arguments:")
+        print("1 argument:")
     else:
         print("{} arguments:".format(num_args))
 
