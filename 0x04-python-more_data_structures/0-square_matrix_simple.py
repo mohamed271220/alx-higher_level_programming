64efbb6e5170706e444cd36829c53721f592001b
mohamed <mohamedaprahym@gmail.com>
2024-03-20 17:47:06 +0200

    f

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,2 @@
+def square_matrix_simple(matrix=[]):
+    return [[element**2 for element in row] for row in matrix]
---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,2 @@
+def search_replace(my_list, search, replace):
+    return [replace if element == search else element for element in my_list]
