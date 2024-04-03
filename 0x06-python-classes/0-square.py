66dc25fb0732bb78ce39db09ae057faf750d9801
mohamed <mohamedaprahym@gmail.com>
2024-04-03 14:11:13 +0200

    d

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-
+"""This is a module that defines a square."""
 
 class Square:
     """This is an empty class that defines a square."""
+    pass
--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""This is a module that defines a square."""
 
 
 class Square:
