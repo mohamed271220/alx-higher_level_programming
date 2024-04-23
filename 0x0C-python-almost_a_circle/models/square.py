7def37602c6108108c6268ee403904a2b819d3e2
mohamed <mohamedaprahym@gmail.com>
2024-04-23 08:57:39 +0200

    s

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -10,6 +10,11 @@ class Square(Rectangle):
         """ Constructor """
         super().__init__(size, size, x, y, id)
 
+    def __str__(self):
+        """ String representation """
+        return '[{}] ({}) {}/{} - {}'.\
+            format(type(self).__name__, self.id, self.x, self.y, self.width)
+
     @property
     def size(self):
         """ Getter """
@@ -20,32 +25,3 @@ class Square(Rectangle):
         """ Setter """
         self.width = value
         self.height = value
-
-    def __str__(self):
-        """ String representation """
-        return '[{}] ({}) {}/{} - {}'.\
-            format(type(self).__name__, self.id, self.x, self.y, self.width)
-
-    def __update(self, id=None, size=None, x=None, y=None):
-        """ Updates """
-        if id is not None:
-            self.id = id
-        if size is not None:
-            self.size = size
-        if x is not None:
-            self.x = x
-        if y is not None:
-            self.y = y
-
-    def update(self, *args, **kwargs):
-        """ Updates square"""
-        if args:
-            self.__update(*args)
-        elif kwargs:
-            self.__update(**kwargs)
-
-
-   def to_dictionary(self):
-        '''Returns dictionary representation of this class.'''
-        return {"id": self.id, "size": self.width,
-                "x": self.x, "y": self.y}
