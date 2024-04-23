94f410071bcd50d2e8e001aca43680344a1dbdaa
mohamed <mohamedaprahym@gmail.com>
2024-04-23 08:58:05 +0200

    s

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -25,3 +25,28 @@ class Square(Rectangle):
         """ Setter """
         self.width = value
         self.height = value
+
+
+    def __update(self, id=None, size=None, x=None, y=None):
+        """ Updates """
+        if id is not None:
+            self.id = id
+        if size is not None:
+            self.size = size
+        if x is not None:
+            self.x = x
+        if y is not None:
+            self.y = y
+
+    def update(self, *args, **kwargs):
+        """ Updates square"""
+        if args:
+            self.__update(*args)
+        elif kwargs:
+            self.__update(**kwargs)
+
+
+   def to_dictionary(self):
+        '''Returns dictionary representation of this class.'''
+        return {"id": self.id, "size": self.width,
+                "x": self.x, "y": self.y}
