06e3c88459c7043c11a33074b367ae697de5e6ed
mohamed <mohamedaprahym@gmail.com>
2024-04-23 08:54:02 +0200

    s

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -9,7 +9,6 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """ Constructor """
         super().__init__(size, size, x, y, id)
-        self.size = size
 
     @property
     def size(self):
@@ -19,18 +18,16 @@ class Square(Rectangle):
     @size.setter
     def size(self, value):
         """ Setter """
-        self.validate_integer("width", value, False)
         self.width = value
         self.height = value
 
     def __str__(self):
         """ String representation """
-        return "[{}] ({}) {}/{} - {}".\
+        return '[{}] ({}) {}/{} - {}'.\
             format(type(self).__name__, self.id, self.x, self.y, self.width)
 
-
     def __update(self, id=None, size=None, x=None, y=None):
-        '''Internal method that updates instance attributes via */**args.'''
+        """ Updates """
         if id is not None:
             self.id = id
         if size is not None:
@@ -41,7 +38,7 @@ class Square(Rectangle):
             self.y = y
 
     def update(self, *args, **kwargs):
-        '''Updates instance attributes via no-keyword & keyword args.'''
+        """ Updates """
         if args:
             self.__update(*args)
         elif kwargs:
