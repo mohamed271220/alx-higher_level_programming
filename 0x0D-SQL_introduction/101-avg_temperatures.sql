72a2df36e0005e224910dccb7c8099fad511ac96
mohamed <mohamedaprahym@gmail.com>
2024-05-21 20:05:22 +0300

    sql

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,2 +1,2 @@
 -- This script computes the average temperature by city and orders the result by temperature in descending order
-SELECT city, AVG(temperature) AS avg_temp FROM temperatures GROUP BY city ORDER BY avg_temp DESC;
+SELECT city, ROUND(AVG(temperature), 4) AS avg_temp FROM temperatures GROUP BY city ORDER BY avg_temp DESC;
