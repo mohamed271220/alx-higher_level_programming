e901ebe177cef77d86c3e70a1b880e1923dc2b40
mohamed <mohamedaprahym@gmail.com>
2024-06-19 12:23:37 +0300

    s

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-exports.eserver = function (list) {
+exports.esrever = function (list) {
   const reversedList = [];
   for (let i = list.length - 1; i >= 0; i--) {
     reversedList.push(list[i]);
