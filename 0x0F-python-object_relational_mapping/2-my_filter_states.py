806a7c86c5dcadabc87d30d8e70f79f9727c0ddd
mohamed <mohamedaprahym@gmail.com>
2024-06-23 10:20:33 +0300

    s

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Module to list all states from a database matching a user-provided name
+"""
+
+import MySQLdb
+import sys
+
+
+def filter_states():
+    """
+    Lists all states in a database matching a user-provided name
+    """
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name = %s"
+                   "ORDER BY id ASC", (sys.argv[4],))
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    filter_states()
